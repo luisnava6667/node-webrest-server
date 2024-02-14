ff5c39e8bd2b4afc9f3422976cc787b780453d7a
luisnava6667 <navaluisrodolfo@gmail.com>
2024-02-14 05:16:44 -0300

    CRUD

---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import { TodosControllers } from './controller'
+
+export class TodoRoutes {
+  static get routes(): Router {
+    const router = Router()
+    const todoControllers = new TodosControllers()
+    router.get('/', todoControllers.getTodos)
+    router.get('/:id', todoControllers.getTodoById)
+    router.post('/', todoControllers.createTodo)
+    router.put('/:id', todoControllers.updateTodo)
+    router.delete('/:id', todoControllers.deleteTodo)
+    return router
+  }
+}
