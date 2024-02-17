014f00fde85a7ab359b107bda47fd78e8cc2076d
luisnava6667 <navaluisrodolfo@gmail.com>
2024-02-17 08:50:10 -0300

    DTO

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 1. Clonar el .env.template y crear el .env
 
-
+2. Ejecutar el comando `docker compose up -d`
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -7,6 +7,7 @@ export class TodoRoutes {
     const todoControllers = new TodosControllers()
     router.get('/', todoControllers.getTodos)
     router.get('/:id', todoControllers.getTodoById)
+    
     router.post('/', todoControllers.createTodo)
     router.put('/:id', todoControllers.updateTodo)
     router.delete('/:id', todoControllers.deleteTodo)
